707b61a4cd1d3dbf95459628c962159e8b292c29
vyeos <rudranpatel0980@gmail.com>
2026-02-15 17:24:42 +0530

    add: watch target

---
+++ VyefitWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VyefitWatch Watch App
+//
+//  Created by Rudra Patel on 15/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VyefitWatch Watch App/VyefitWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VyefitWatchApp.swift
+//  VyefitWatch Watch App
+//
+//  Created by Rudra Patel on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct VyefitWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
